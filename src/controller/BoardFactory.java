25c1c183bd4a753cad4975b561bf2260416e1fdc
Riza Oguz Ongun <rizaoguz.ongun@postgrad.manchester.ac.uk>
2018-03-25 23:50:16 +0300

    Factory for creating boards with Singleton has been added

---
+++ src/controller/BoardFactory.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import model.*;
+
+/**
+ *
+ * @author rizaoguzongun
+ */
+public class BoardFactory {
+
+    private static BoardFactory boardFactory;
+
+    private BoardFactory() {
+
+    }
+
+    public static BoardFactory getBoardFactory() {
+        if (boardFactory == null) {
+            boardFactory = new BoardFactory();
+        }
+        return boardFactory;
+    }
+
+    public Board createBoard(String boardSize) {
+
+        switch (boardSize) {
+            case "small":
+                SmallBoard smallBoard = new SmallBoard();
+                smallBoard.setParameters();
+                return smallBoard;
+            case "medium":
+                MediumBoard mediumBoard = new MediumBoard();
+                mediumBoard.setParameters();
+                return mediumBoard;
+            case "large":
+                LargeBoard largeBoard = new LargeBoard();
+                largeBoard.setParameters();
+                return largeBoard;
+        }
+        //If invalid value is given, mediumBoard will be returned
+        MediumBoard mediumBoard = new MediumBoard();
+        mediumBoard.setParameters();
+        return mediumBoard;
+
+    }
+
+}
