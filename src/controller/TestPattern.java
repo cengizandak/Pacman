ec7e30191a32086eace6554202596ee66bc0078f
Christos Panagi <christos.panagi@postgrad.manchester.ac.uk>
2018-03-27 22:22:50 +0300

    Tested TestPattern for Strategy and Iterator

--- src/controller/TestPattern.java
+++ src/controller/TestPattern.java
@@ -5,8 +5,8 @@
  */
 package controller;
 
-import model.GameData;
 import model.GameData.*;
+import model.*;
 
 /**
  *
@@ -37,5 +37,26 @@ public class TestPattern {
             //...  Add other states 
         }
         gameContext.getGs().display();
+        
+        Pacman pacman = new Pacman(1,1,1,Pacman.State.NORMAL, 3);
+        SmallBoard board = new SmallBoard();
+        board.setParameters();
+        ContextStrategy contextStrategy = new ContextStrategy(new Level1());
+        Ghost g[] = new Ghost[board.getNumberOfGhosts()];
+        g[0]  = new Ghost(1,22,Ghost.State.ALIVE);
+        g[1] =new Ghost(2,22,Ghost.State.ALIVE);
+        g[2] =new Ghost(3,22,Ghost.State.ALIVE);
+        g[3] =new Ghost(4,22,Ghost.State.ALIVE);
+        GhostRepository ghosts = new GhostRepository(g);
+        
+        for(Iterator iter = ghosts.getIterator(); iter.hasNext();){
+            Ghost ghost = (Ghost)iter.next();
+            System.out.println(ghost.getCoordinateX()+" "+ghost.getCoordinateY());
+            contextStrategy.executeStrategy(ghost, pacman, board);
+            System.out.println(ghost.getCoordinateX()+" "+ghost.getCoordinateY());
+         
+      } 
     }
+    
+   
 }
